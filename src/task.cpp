0da9f7f99811df94001c36958c00638e56c10cb1
Raffaele Mazza <raffaele.mazza1@studenti.unicampania.it>
2024-03-20 14:49:25 +0100

    first commit

--- src/task.cpp
+++ src/task.cpp
@@ -4,6 +4,7 @@
 #include "yaskawa_cross_modal/CartesianTrajAction.h"
 #include "std_srvs/SetBool.h"
 #include <tf2_ros/transform_listener.h>
+#include <sun_robot_ros/RobotMotionClient.h>
 
 bool askContinue(const std::string &prompt = "")
 {
@@ -30,8 +31,18 @@ bool askContinue(const std::string &prompt = "")
 int main(int argc, char *argv[])
 {
     ros::init(argc,argv,"task");
-    //RobotMotionClient robot(ros::NodeHandle(nh, "motoman"));
+    ros::NodeHandle nh;
     sun::RobotMotionClient robot(ros::NodeHandle(nh, "motoman"));
+    //robot.waitForServers();
     
+    geometry_msgs::Pose posa;
+    posa.position.x = 0;
+    posa.position.y = -0.18;
+    posa.position.z = 0.25;
 
+    if(askContinue("Prova")){
+        robot.goTo(posa, ros::Duration(5.0));
+        ROS_INFO_STREAM("Posa raggiunta");
+    }
+    
 }
