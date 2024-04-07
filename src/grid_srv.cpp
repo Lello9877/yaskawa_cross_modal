d9561789f41c87a5547efa031bda48d887a188b5
Raffaele Mazza <raffaele.mazza1@studenti.unicampania.it>
2024-04-07 18:45:36 +0200

    Grid srv

---
+++ src/grid_srv.cpp
@@ -0,0 +1,65 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PoseArray.h>
+#include "yaskawa_cross_modal/grid.h"
+
+geometry_msgs::PoseArray grid;
+bool call_arrived = false;
+
+bool srv_cb(yaskawa_cross_modal::grid::Request &req, yaskawa_cross_modal::grid::Response &res) {
+
+    geometry_msgs::PoseArray temp_grid;
+    geometry_msgs::Pose initial;
+    double deltax, deltay;
+
+    deltax = (req.xf-req.x0)/req.divx;
+    deltay = (req.yf-req.y0)/req.divy;
+
+    initial.position.z = 0;
+    initial.orientation.w = 0;
+    initial.orientation.x = 0;
+    initial.orientation.y = 1;
+    initial.orientation.z = 0;
+
+    // Costruisco la griglia
+    for(int i = 0; i < req.divy; i++) {
+        initial.position.y = req.y0 + i*deltay;
+        for(int j = 0; j < req.divx; j++) {
+            initial.position.x = req.x0 + j*deltax;
+            temp_grid.poses.push_back(initial);
+        }
+    }
+
+    call_arrived = true;
+    grid = temp_grid;
+    grid.header.frame_id = "base_link";
+    res.grid = grid;
+    if(res.grid.poses.size() == (req.divx*req.divy)) return true;
+    else return false;
+
+}
+
+int main(int argc, char* argv[]) {
+
+    ros::init(argc,argv,"grid_srv");
+    ros::NodeHandle nh;
+    ros::ServiceServer service = nh.advertiseService("grid_srv",srv_cb);
+    ros::Publisher pub_poses = nh.advertise<geometry_msgs::PoseArray>("/grid_poses",1);
+    ros::Rate loop_rate(50.0);
+
+    while(ros::ok() && !call_arrived) {
+        ros::spinOnce();
+        loop_rate.sleep(); 
+    }
+
+    grid.header.frame_id = "base_link";
+
+    while(ros::ok()) {
+        grid.header.stamp = ros::Time::now();
+        pub_poses.publish(grid);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+
+}
--- src/task.cpp
+++ src/task.cpp
@@ -7,6 +7,7 @@
 #include <sun_robot_ros/RobotMotionClient.h>
 #include <sun_robot_ros/ClikClient.h>
 #include <geometry_msgs/PoseArray.h>
+#include "yaskawa_cross_modal/grid.h"
 
 bool askContinue(const std::string &prompt = "")
 {
@@ -27,6 +28,10 @@ bool askContinue(const std::string &prompt = "")
     throw std::runtime_error("USER STOP!");
 }
 
+void grid_cb(const geometry_msgs::PoseArrayPtr& msg){
+    
+}
+
 int main(int argc, char *argv[])
 {
     ros::init(argc,argv,"task");
@@ -36,27 +41,19 @@ int main(int argc, char *argv[])
 
     geometry_msgs::Pose start, posa, end_effector;
 
-    start.position.x = 0.30;
-    start.position.y = -0.30;
-    start.position.z = 0.24;
     // una buona posa, in 15 s
     // posa.position.x = 0.60;
     // posa.position.y = 0.10;
     // posa.position.z = 0.30;
+
+    start.position.x = 0.30;
+    start.position.y = -0.30;
+    start.position.z = 0.24; 
     start.orientation.w = 0;
     start.orientation.x = 0;
     start.orientation.y = 1;
     start.orientation.z = 0;
-    //const std::vector<double> qf = {-1.53, -0.42, 0, -0.52, 0, -0.94, 1.13};
     const std::vector<double> q0 = {0, 0, 0, 0, 0, 0, 0};
-
-    posa.position.x = 0.40;
-    posa.position.y = 0.10;
-    posa.position.z = 0.05;
-    posa.orientation.w = 0;
-    posa.orientation.x = 0;
-    posa.orientation.y = 1;
-    posa.orientation.z = 0;
 
     tf2_ros::Buffer tfBuffer;
     tf2_ros::TransformListener tfListener(tfBuffer);
@@ -87,70 +84,38 @@ int main(int argc, char *argv[])
         ROS_INFO_STREAM("Posa in cartesiano raggiunta");
     }
 
-    //     if(askContinue("Discesa lungo z")) {
-    //     robot.goTo(posa, ros::Duration(5.0));
-    //     ROS_INFO_STREAM("Posa in cartesiano raggiunta");
-    // }
-
     // ESPLORAZIONE
 
-    double x0, y0, z0, z;
-    double xf, yf, zf;
-    double divx, divy, deltax, deltay, deltaz;
-
-    x0 = 0.30;
-    y0 = -0.30;
-    z0 = 0.21;
-    xf = 0.45;
-    yf = 0.30;
-    divx = 3;
-    divy = 15;
-    deltax = (xf-x0)/divx;
-    deltay = (yf-y0)/divy;
-    deltaz = -0.03;
-
-    //std::vector<geometry_msgs::Pose> grid;
     geometry_msgs::PoseArray grid;
-    geometry_msgs::Pose initial, esplor;
+    ros::ServiceClient grid_client = nh.serviceClient<yaskawa_cross_modal::grid>("grid_srv");
+    yaskawa_cross_modal::grid srv;
+    srv.request.x0 = 0.30;
+    srv.request.xf = 0.45;
+    srv.request.y0 = -0.30;
+    srv.request.yf = 0.30;
+    srv.request.divx = 3;
+    srv.request.divy = 15;
+    if(grid_client.call(srv)) { grid = srv.response.grid; ROS_INFO("Griglia ottenuta"); }
+    else { ROS_INFO("Errore nella generazione della griglia"); }
+    //std::cout << grid.poses.size();
 
-    initial.position.z = z0;
-    initial.orientation.w = 0;
-    initial.orientation.x = 0;
-    initial.orientation.y = 1;
-    initial.orientation.z = 0;
+    double deltaz = -0.03;
+    geometry_msgs::Pose esplor;
 
-    // Costruisco la griglia
-    for(int i = 0; i < divy; i++) {
-        initial.position.y = y0 + i*deltay;
-        for(int j = 0; j < divx; j++) {
-            initial.position.x = x0 + j*deltax;
-            grid.poses.push_back(initial);
-        }
-    }
-
-    std::cout << grid.poses.size();
-    std::cout << grid.poses[44];
-
-    // ros::Publisher pub_poses = nh.advertise<geometry_msgs::PoseArray>("/grid",1);
-    // while(ros::ok())
-    //     pub_poses.publish(grid);
-
-    // for(int i = 0; i < grid.size(); i++)
-    //     std::cout << grid[i].position << std::endl;
-
-    for(int i = 0; i < grid.poses.size(); i++) {
-        if(askContinue("Prossima posa di esplorazione")) {
-            z = z0;
-            esplor = grid.poses[i];
-            do
-            {   
-                if(askContinue("Esplora")) { robot.goTo(esplor, ros::Duration(5.0)); ROS_INFO_STREAM("Posa raggiunta"); }
-                //{condizione di tocco del cavo, altrimenti scendi ancora}
-                z = z + deltaz;
-                esplor.position.z = z;
-                std::cout << z << std::endl;
-            } while(ros::ok() && z > 0.13);
-        }
-        else { break; } 
-    }
+    // for(int i = 0; i < grid.poses.size(); i++) {
+    //     if(askContinue("Prossima posa di esplorazione")) {
+    //         z = z0;
+    //         esplor = grid.poses[i];
+    //         esplor.position.z = z;
+    //         do
+    //         {   
+    //             if(askContinue("Esplora")) { robot.goTo(esplor, ros::Duration(5.0)); ROS_INFO_STREAM("Posa raggiunta"); }
+    //             //{condizione di tocco del cavo, altrimenti scendi ancora}
+    //             z = z + deltaz;
+    //             esplor.position.z = z;
+    //             std::cout << z << std::endl;
+    //         } while(ros::ok() && z > 0.13);
+    //     }
+    //     else { break; } 
+    // }
 }
